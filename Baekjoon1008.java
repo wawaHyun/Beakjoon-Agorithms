7e6b290c136592ae3be20bf565a71d0d0f445a61
wawaHyun <sjrkchdkdy@gmail.com>
2024-02-18 15:58:41 +0900

    Baekjoon2

---
+++ Baekjoon1008.java
@@ -0,0 +1,15 @@
+import java.util.*;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        //double result = 0;
+        double result = a/b;
+
+        System.out.printf("%.9f",result);
+    }
+}
